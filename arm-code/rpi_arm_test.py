89a33527723e86f403b37ea8eac18fee2cc61e1c
OvAlexander <alexanderleeov@gmail.com>
2024-03-20 19:21:01 -0700

    revert back 1 commit

--- arm-code/rpi_arm_test.py
+++ arm-code/rpi_arm_test.py
@@ -71,7 +71,8 @@ def pullout():
 
      #reach out further
     print("move 4")
-    motor.simMotorRun([145, 130, 235], [2, 3, 4])
+    #almost good values motor.simMotorRun([140, 127, 233], [2, 3, 4])
+    motor.simMotorRun([140, 127, 233], [2, 3, 4])
     checkMovement(MOVE_IDs)
     print("move fin")
     time.sleep(2)
