8f95da7f745d0034d9b04ba45ec4386e301199fc
OvAlexander <alexanderleeov@gmail.com>
2024-03-22 15:43:40 -0700

    Added move 8.75 to move motor 4 back motor

--- arm-code/rpi_arm_test.py
+++ arm-code/rpi_arm_test.py
@@ -173,11 +173,11 @@ def pullout():
     print("move fin")
     time.sleep(2)
 
-    motor.dxlSetVelo([40,10], [2,4])  # ALWAYS SET SPEED BEFORE ANYTHING
+    motor.dxlSetVelo([40,30], [2,4])  # ALWAYS SET SPEED BEFORE ANYTHING
 
     #pull all the way back
     print("move 8.75")
-    motor.simMotorRun([290], [2])
+    motor.simMotorRun([120], [4])
     checkMovement(MOVE_IDs)
     print("move fin")
     time.sleep(2)
