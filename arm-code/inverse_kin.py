2421f177273fc6f971e44f14c064b8e85120b126
OvAlexander <alexanderleeov@gmail.com>
2024-03-25 18:27:52 -0700

    test:programming GCS pullout

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -107,7 +107,8 @@ time.sleep(2)
 # motor.simMotorRun([30], [0])  # Reset claw looking up
 # time.sleep(2)
 
-test_angle = calculation.angle_Calc([150,0,100], 0)
+# test_angle = calculation.angle_Calc([150,0,100], 0) might work for return to home
+test_angle = calculation.angle_Calc([300,0,75], 0)
 print(test_angle)
 print("move 4 pull away")
 motor.simMotorRun(test_angle, [1, 2, 3, 4])
