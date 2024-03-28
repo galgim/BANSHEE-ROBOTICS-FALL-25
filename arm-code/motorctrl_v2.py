eef9ef808bc5ffda6df0a976f862294cf1704c75
OvAlexander <alexanderleeov@gmail.com>
2024-03-27 20:48:06 -0700

    fix: added time between move functionality

--- arm-code/motorctrl_v2.py
+++ arm-code/motorctrl_v2.py
@@ -345,8 +345,8 @@ def simWrite(dxl_goal_inputs, dxlIDs):
     #Create parameter storage for present positions
     for id in range(idNum):
         dxl_addparam_result = motor_sync_read.addParam(dxlIDs[id])
-        if dxl_addparam_result != True:
-            print("[ID:%03d] groupSyncRead addparam failed" % dxlIDs[id])
+        # if dxl_addparam_result != True:
+        #     print("[ID:%03d] groupSyncRead addparam failed" % dxlIDs[id])
         #print("DXL_ADDPARAM_RESULT is " ,dxl_addparam_result)
 
     param_goal_position = [0] * idNum
@@ -359,14 +359,14 @@ def simWrite(dxl_goal_inputs, dxlIDs):
     for id in range(idNum): 
     #Add goal position input values of bicep and forearm motors to Syncwrite parameter storage
         dxl_addparam_result = motor_sync_write.addParam(dxlIDs[id], param_goal_position[id])
-        if dxl_addparam_result != True:
-            print("[ID:%03d] groupSyncWrite addparam failed" % dxlIDs[id])
+        # if dxl_addparam_result != True:
+        #     print("[ID:%03d] groupSyncWrite addparam failed" % dxlIDs[id])
         #print("groupSyncWrite for [ID:%03d] works" % (DXL_ID[device_index]))
    
     #Syncwrite goal position to bicep and forearm motors
     dxl_comm_result = motor_sync_write.txPacket()
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    # if dxl_comm_result != COMM_SUCCESS:
+    #     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 
     #Clear syncwrite parameter storage
     motor_sync_write.clearParam()
@@ -405,14 +405,14 @@ def simPosCheck(dxl_goal_inputs, dxlIDs):
         dxl_present_position = [0] * idNum
         # Syncread present position
         dxl_comm_result = motor_sync_read.txRxPacket()
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        # if dxl_comm_result != COMM_SUCCESS:
+            # print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 
         # Check if groupsyncread data of Dynamixel is available
         for motorID in dxlIDs:
             dxl_getdata_result = motor_sync_read.isAvailable(motorID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
-            if dxl_getdata_result != True:
-                print("[ID:%03d] groupSyncRead getdata failed" % motorID)
+            # if dxl_getdata_result != True:
+            #     print("[ID:%03d] groupSyncRead getdata failed" % motorID)
 
         # Get Dynamixel present position value
         for id in range(idNum):
