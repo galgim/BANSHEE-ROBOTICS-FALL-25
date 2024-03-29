12e7082cdb2f004a8726ad86a025d81c670cdd82
YeachanOkh <yeachanokh@gmail.com>
2024-03-28 17:34:03 -0700

    livestream test 10

--- new-btp-movement-code/livestream.py
+++ new-btp-movement-code/livestream.py
@@ -9,15 +9,18 @@ s.bind(('', port))
 print("Listening at:", s)
 
 while True:
-      x = s.recvfrom(1000000)
-      data = x[0]
-      print("streaming")
-      data = pickle.loads(data)
-      data = cv2.imdecode(data, cv2.IMREAD_COLOR)
-      cv2.imshow('server', data)
-      
-      key = cv2.waitKey(1)
-      if key == 27:
-        break
+    try:
+        x = s.recvfrom(1000000)
+        data = x[0]
+        print("Streaming")
+        data = pickle.loads(data)
+        data = cv2.imdecode(data, cv2.IMREAD_COLOR)
+        cv2.imshow('server', data)
+        
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    except Exception as e:
+        print("Error:", e)
 
 cv2.destroyAllWindows()
