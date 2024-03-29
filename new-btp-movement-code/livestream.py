24e19e5412e4cae3195a129c62b49c8ca621704c
YeachanOkh <yeachanokh@gmail.com>
2024-03-28 19:08:37 -0700

    camera test

--- new-btp-movement-code/livestream.py
+++ new-btp-movement-code/livestream.py
@@ -1,26 +1,22 @@
 import cv2
 import socket
 import pickle
-
+import numpy as np
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ip = "192.168.1.89"
 port = 9999  # Change the port number
-s.bind(('', port))
+s.bind((ip, port))
 print("Listening at:", s)
 
 while True:
-    try:
-        x = s.recvfrom(1000000)
-        data = x[0]
-        data = pickle.loads(data)
-        data = cv2.imdecode(data, cv2.IMREAD_COLOR)
-        print("data",data)
-        cv2.imshow('server', data)
-        
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
-    except Exception as e:
-        print("Error:", e)
-
+    x = s.recvfrom(1000000)
+    data = x[0]
+    data = pickle.loads(data)
+    data = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    print("data",data)
+    cv2.imshow('server', data)
+    
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
 cv2.destroyAllWindows()
--- new-btp-movement-code/stream.py
+++ new-btp-movement-code/stream.py
@@ -1,28 +1,63 @@
+# import cv2
+# import socket
+# import pickle
+# import numpy as np
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1000000)
+# serverip = "192.168.1.89"
+# serverport = 9999  # Make sure this matches the port the server is listening on
+# cap = cv2.VideoCapture(0)
+# cap.set(3,640)
+# cap.set(4,480)
+# print("sending at:", s)
+# while True:
+#     ret, photo = cap.read()
+#     cv2.imshow('streaming', photo)
+#     ret, buffer = cv2.imencode(".jpg", photo, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
+#     x_as_bytes = pickle.dumps(buffer)
+#     s.sendto(x_as_bytes, (serverip, serverport))
+#     key = cv2.waitKey(1)
+#     if key == 27:
+#         break
+
+# cv2.destroyAllWindows()
+# cap.release()
 import cv2
 import socket
 import pickle
+from realsense_depth import DepthCamera  # Assuming this is your custom class for capturing depth frames
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1000000)
-serverip = "192.168.1.89"
-serverport = 9999  # Make sure this matches the port the server is listening on
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-print("sending at:", s)
-while True:
-    ret, photo = cap.read()
-    cv2.imshow('streaming', photo)
-    ret, buffer = cv2.imencode(".jpeg", photo, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
-    x_as_bytes = pickle.dumps(buffer)
-    s.sendto(x_as_bytes, (serverip, serverport))
-    
-    # Introduce a delay of 30 milliseconds between sending frames
-    cv2.waitKey(30)
-    
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+class ArDetector:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10000000)
+        self.server_ip = "192.168.1.89"
+        self.server_port = 9998
+        self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_100)
+        self.parameters = cv2.aruco.DetectorParameters_create()
+        self.video_capture = DepthCamera()  # Use your custom class for capturing depth frames
 
-cv2.destroyAllWindows()
-cap.release()
+    def run(self):
+        while True:
+            ret, depth_frame, color_frame = self.video_capture.get_frame()
+            ret, buffer = cv2.imencode(".jpg", color_frame, [int(cv2.IMWRITE_JPEG_QUALITY), 60])
+            data = pickle.dumps(buffer)
+            self.s.sendto(data, (self.server_ip, self.server_port))
+
+            # Your AR marker detection and processing code here
+            # ...
+
+            cv2.imshow('ArUco Marker Detection with Confidence', color_frame)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+
+        # Release resources before exiting
+        self.video_capture.release()
+        cv2.destroyAllWindows()
+        self.s.close()
+
+if __name__ == "__main__":
+    detector = ArDetector()
+    detector.run()
