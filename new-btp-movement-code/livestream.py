6104f0f88f256440254d6ab217dce7a63f05eacd
YeachanOkh <yeachanokh@gmail.com>
2024-03-28 17:29:21 -0700

    livesream test 9

--- new-btp-movement-code/livestream.py
+++ new-btp-movement-code/livestream.py
@@ -11,6 +11,7 @@ print("Listening at:", s)
 while True:
       x = s.recvfrom(1000000)
       data = x[0]
+      print("streaming")
       data = pickle.loads(data)
       data = cv2.imdecode(data, cv2.IMREAD_COLOR)
       cv2.imshow('server', data)
