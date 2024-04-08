507452a1453c58ca7bf2ec029af358278d6ed684
OvAlexander <alexanderleeov@gmail.com>
2024-04-08 16:12:30 -0700

    feat: created new version of local full cycle script

---
+++ arm-code/fullcycle_local_v2.py
@@ -0,0 +1,257 @@
+import math
+import motorctrl_v1 as motor
+import Movement_Calc_v2 as calculation
+import numpy as np
+import time
+import serial
+import cv2
+import socket
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(22, GPIO.OUT)
+GPIO.output(22, GPIO.LOW)
+
+BASE_ID = 1
+BICEP_ID = 2
+FOREARM_ID = 3
+WRIST_ID = 4
+CLAW_ID = 0
+
+
+# PORT_NUM = '/dev/cu.usbserial-FT5NY9DI'  #for mac
+PORT_NUM = '/dev/ttyUSB0'  # for rpi
+ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)  # for rpi
+
+BAUDRATE = 1000000
+MOVEARM_MODE = 1
+ADDR_PRESENT_POSITION = 132
+ALL_IDs = [BASE_ID, BICEP_ID, FOREARM_ID, WRIST_ID, CLAW_ID]
+MOVE_IDs = [BASE_ID, BICEP_ID, FOREARM_ID, WRIST_ID, CLAW_ID]
+
+motor.portInitialization(PORT_NUM, ALL_IDs)
+
+def debug_gcs_pullout():
+    start_time = time.time()
+    motor.dxlSetVelo([25, 25, 25, 25, 25], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
+    time.sleep(0.1)
+    print("set up move")
+    motor.simMotorRun([110, 223, 270, 47, 272], [0, 1, 2, 3, 4])  # Reset claw looking up
+    time.sleep(2)
+
+    print("move 1 move to chamber")
+    motor.simMotorRun(max_length_angle, [1, 2, 3, 4])
+    time.sleep(0.4)
+
+    print("move 2 pitch wrist")
+    motor.simMotorRun([200], [4])  # Reset claw looking up
+    time.sleep(0.5)
+
+    print("move 3 close grip")
+    motor.simMotorRun([30], [0])  # Reset claw looking up
+    time.sleep(0.1)
+
+    print("move 4 pull forearm back")
+    motor.simMotorRun([160], [3])  # Reset claw looking up
+    time.sleep(0.15)
+
+    inital_pull_out_angle = calculation.angle_Calc([300,0,60], 0)
+    print("move 4 pull away slight")
+    motor.simMotorRun(inital_pull_out_angle, [1, 2, 3, 4])
+    time.sleep(0.15)
+
+    second_pull_out_angle = calculation.angle_Calc([250,0,60], 0)
+    print("move 5 pull away more")
+    motor.simMotorRun(second_pull_out_angle, [1, 2, 3, 4])
+    time.sleep(0.3)
+
+    final_pull_out_angle = calculation.angle_Calc([200,0,60], 0)
+    print("move 6 pull away even more")
+    motor.simMotorRun(final_pull_out_angle, [1, 2, 3, 4])
+    time.sleep(0.6)
+
+    print("move 7 pull forearm back")
+    motor.simMotorRun([45], [3])  # Reset claw looking up
+    time.sleep(0.8)
+
+    print("move 8 pull forearm back")
+    motor.simMotorRun([200], [2])  # Reset claw looking up
+    time.sleep(0.25)
+
+    print("move 9 pull forearm back")
+    motor.simMotorRun([220], [4])  # Reset claw looking up
+    time.sleep(0.4)
+
+    motor.dxlSetVelo([15, 15, 15, 15, 15], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
+    time.sleep(0.1)
+
+    print("move 13 pull forearm back")
+    motor.simMotorRun([265, 47, 170], [2, 3, 4])
+    time.sleep(0.8)
+
+    print("move 14 pull forearm back")
+    motor.simMotorRun([270], [4])
+    end_time = time.time()
+    print(end_time-start_time)
+
+    start_time = time.time()
+    ctrl.dxlSetVelo([25, 25, 25, 25, 25], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
+    time.sleep(0.1)
+    print("set up move")
+    ctrl.motorRun([110, 223, 270, 47, 272], [0, 1, 2, 3, 4])  # Reset claw looking up
+    time.sleep(2)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    print("move 1 move to chamber")
+    ctrl.motorRun(max_length_angle, [1, 2, 3, 4])
+    time.sleep(0.4)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    print("move 2 pitch wrist")
+    ctrl.motorRun([200], [4])  # Reset claw looking up
+    time.sleep(0.5)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    print("move 3 close grip")
+    ctrl.motorRun([30], [0])  # Reset claw looking up
+    time.sleep(0.1)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    print("move 4 pull forearm back")
+    ctrl.motorRun([160], [3])  # Reset claw looking up
+    time.sleep(0.15)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    initial_pull_out_angle = calculation.angle_Calc([300,0,60], 0)
+    print("move 4 pull away slight")
+    ctrl.motorRun(initial_pull_out_angle, [1, 2, 3, 4])
+    time.sleep(0.15)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    second_pull_out_angle = calculation.angle_Calc([250,0,60], 0)
+    print("move 5 pull away more")
+    ctrl.motorRun(second_pull_out_angle, [1, 2, 3, 4])
+    time.sleep(0.3)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    final_pull_out_angle = calculation.angle_Calc([200,0,60], 0)
+    print("move 6 pull away even more")
+    ctrl.motorRun(final_pull_out_angle, [1, 2, 3, 4])
+    time.sleep(0.6)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    print("move 7 pull forearm back")
+    ctrl.motorRun([45], [3])  # Reset claw looking up
+    time.sleep(0.8)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    print("move 8 pull forearm back")
+    ctrl.motorRun([200], [2])  # Reset claw looking up
+    time.sleep(0.25)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    print("move 9 pull forearm back")
+    ctrl.motorRun([220], [4])  # Reset claw looking up
+    time.sleep(0.4)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    ctrl.dxlSetVelo([15, 15, 15, 15, 15], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
+    time.sleep(0.1)
+
+    print("move 13 pull forearm back")
+    ctrl.motorRun([265, 47, 170], [2, 3, 4])
+    time.sleep(0.8)
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+
+    print("move 14 pull forearm back")
+    ctrl.motorRun([270], [4])
+    ctrl.dxlPresPos([0, 1, 2, 3, 4])
+    end_time = time.time()
+    print(end_time-start_time)
+
+def debug_gcs_push_in():
+    #Push In Battery
+    start_time = time.time()
+    motor.dxlSetVelo([15, 15, 15, 15, 15], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
+    time.sleep(0.1)
+
+    print("move back to chamber")
+    motor.simMotorRun([180, 62], [2, 3])
+    time.sleep(2.5)
+
+    motor.dxlSetVelo([50, 50, 50, 50, 50], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
+    time.sleep(0.1)
+
+    gcs_push_in_angle = calculation.angle_Calc([310,0,65], 0)
+    print(gcs_push_in_angle)
+    print("push in to chamber")
+    motor.simMotorRun(gcs_push_in_angle, [1, 2, 3, 4])
+    time.sleep(0.15)
+
+    print("push all the way in to chamber")
+    motor.simMotorRun(max_length_angle, [1, 2, 3, 4])
+    time.sleep(0.15)
+
+    print("open claw")
+    motor.simMotorRun([110], [0])  # Reset claw looking up
+    time.sleep(0.15)
+
+    gcs_pull_out_angle = calculation.angle_Calc([300,0,60], 0)
+    print("move 4 pull away slight")
+    motor.simMotorRun(gcs_pull_out_angle, [1, 2, 3, 4])
+    time.sleep(1.5)
+
+    print("set up move")
+    motor.simMotorRun([110, 223, 270, 47, 272], [0, 1, 2, 3, 4])  # Reset claw looking up
+    end_time = time.time()
+    print(end_time-start_time)
+
+arduinoinput = ''
+
+# Take Battery from GCS
+debug_gcs_pullout()
+
+print("Start Arduino Code")
+GPIO.output(18, GPIO.HIGH)
+time.sleep(8)
+ser.write(b'b')  # Tell Arduino it's good to go
+
+# Wait for arduino to send s, means it has arrived at BVM
+while True:
+    print("waiting for s")
+    response = ser.readline().strip()
+    print(arduinoinput)
+    arduinoinput = response.decode()
+    if arduinoinput == '1':
+        print("push battery into BVM!")
+        break
+
+
+# Push battery into BVM
+ser.flush()
+time.sleep(3)
+debug_gcs_push_in()
+
+time.sleep(5)  # let BVM cycle battery
+
+# Take battery out of BVM
+debug_gcs_pullout()
+time.sleep(3)
+print("sending b to arduino")
+ser.write(b'g')  # Tell Arduino it's good to go
+time.sleep(0.5)
+response = ser.readline().strip().decode()
+print(response)
+# Wait for arduino to send s, means it has arrived at GCS
+while True:
+    response = ser.readline().strip()
+    arduinoinput = response.decode()
+    if arduinoinput == '1':
+        print("push battery into GCS!")
+        break
+
+
+# Push battery into GCS
+
+debug_gcs_push_in()
+GPIO.cleanup()
