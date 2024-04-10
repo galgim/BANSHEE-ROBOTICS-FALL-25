060c310dc80310f78cd796edc93dff94197a0543
OvAlexander <alexanderleeov@gmail.com>
2024-04-10 14:53:10 -0700

    test: added pull away code

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -335,14 +335,14 @@ def debug_bvm_push_in():
     motor.dxlPresPos([0, 1, 2, 3, 4])
 
     print("move 6 pull away more")
-    motor.simMotorRun([225, 180, 50, 269], [1, 2, 3, 4])
+    motor.simMotorRun([225, 180, 60, 269], [1, 2, 3, 4])
     time.sleep(0.3)
     motor.dxlPresPos([0, 1, 2, 3, 4])
     
     for i in range(180,270,5):
         print(i)
         print("move 7 pull away more")
-        motor.simMotorRun([225, i, 50, 269], [1, 2, 3, 4])
+        motor.simMotorRun([225, i, 60, 269], [1, 2, 3, 4])
         time.sleep(0.05)
         motor.dxlPresPos([0, 1, 2, 3, 4])
 
