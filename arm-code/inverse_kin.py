ce85252d031eff81966798f20b83fa29862dbcb2
OvAlexander <alexanderleeov@gmail.com>
2024-04-10 17:56:37 -0700

    test: lowered amount of pull out

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -388,11 +388,11 @@ def debug_bvm_push_in():
     print("push in to chamber")
     motor.simMotorRun(gcs_push_in_angle, [1, 2, 3, 4])
     time.sleep(0.15)
-
+    #[225, 160, 78, 266]
     # gcs_push_in_angle = calculation.angle_Calc([250,0,65], 0)
     # print(gcs_push_in_angle)
     print("push in to chamber")
-    motor.simMotorRun([225, 160, 150, 266], [1, 2, 3, 4])
+    motor.simMotorRun([225, 130, 150, 266], [1, 2, 3, 4])
     time.sleep(0.15)
 
     # gcs_push_in_angle = calculation.angle_Calc([300,0,65], 0)
