e1ca39b15f3acb738fab5921f6c9c3a5472398c8
OvAlexander <alexanderleeov@gmail.com>
2024-04-10 17:35:38 -0700

    test: added pull away code

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -381,8 +381,9 @@ def debug_bvm_push_in():
     print("move back to chamber")
     motor.simMotorRun([180, 62], [2, 3])
     time.sleep(0.5)
+    motor.dxlPresPos([0, 1, 2, 3, 4])
 
-    gcs_push_in_angle = calculation.angle_Calc([310,0,65], 0)
+    gcs_push_in_angle = calculation.angle_Calc([180,0,65], 0)
     print(gcs_push_in_angle)
     print("push in to chamber")
     motor.simMotorRun(gcs_push_in_angle, [1, 2, 3, 4])
