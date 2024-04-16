8bbcc44595d90e9649de73f207223c3687f28931
Caden-Hong <Cadenhong@gmail.com>
2024-04-15 19:15:38 -0700

    updated server ip

--- new-btp-movement-code/ardetectdistance.py
+++ new-btp-movement-code/ardetectdistance.py
@@ -10,7 +10,7 @@ import socket
 class ardetect:
     s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     #s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1000000)
-    server_ip="192.168.1.52"
+    server_ip="192.168.1.95"
     server_port=3000
 
     # 1. Load the ArUco dictionary
