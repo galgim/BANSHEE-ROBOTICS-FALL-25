7c28941921c302b3b3384f7131e4229f5fac7d78
OvAlexander <alexanderleeov@gmail.com>
2024-04-17 15:06:41 -0700

    test: added back bvm push in

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -324,12 +324,12 @@ def debug_bvm_pull_out():
     # motor.simMotorRun([225, 180, 199, 199], [1, 2, 3, 4])
     # time.sleep(0.15)
     # motor.dxlPresPos([0, 1, 2, 3, 4])
-    for i in range(370,190,-5):
+    for i in range(370,190,-10):
         print(i)
         initial_pull_out_angle = calculation.angle_Calc([i,0,67], 0)
         print("move 4 pull away slight")
         motor.simMotorRun(initial_pull_out_angle, [1, 2, 3, 4])
-        time.sleep(0.05)
+        time.sleep(0.3)
         motor.dxlPresPos([0, 1, 2, 3, 4])
 
     print("move 5 pull away more")
