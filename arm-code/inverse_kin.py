a710ba15212ab4c32ba3081bcee321c263b68772
OvAlexander <alexanderleeov@gmail.com>
2024-04-17 15:39:47 -0700

    test: added back bvm push in

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -445,7 +445,7 @@ def debug_bvm_push_in():
     motor.simMotorRun(gcs_push_in_angle, [1, 2, 3, 4])
     time.sleep(0.5)
 
-    motor.simMotorRun([225, 81, 236, 183], [1, 2, 3, 4])
+    motor.simMotorRun([225, 81, 236, 186], [1, 2, 3, 4])
     time.sleep(0.5)
 
     motor.simMotorRun([225], [0])
