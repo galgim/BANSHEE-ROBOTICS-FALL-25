1487cd44396b9d7ced6c1b3b5b540b5c5c3daa93
YeachanOkh <yeachanokh@gmail.com>
2024-04-19 15:16:15 -0700

    gcs push in

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -259,12 +259,12 @@ def debug_gcs_push_in():
     motor.dxlSetVelo([15, 15, 15, 15, 15], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
     time.sleep(0.1)
     #265, 47, 170
-    print("move back to chamber")
+    print("move back to chamber1")
     motor.simMotorRun([256, 54], [2, 3])
     time.sleep(2.5)
 
-    print("move back to chamber")
-    motor.simMotorRun([180, 62], [2, 3])
+    print("move back to chamber2")
+    motor.simMotorRun([180, 56], [2, 3])
     time.sleep(2.5)
 
     motor.dxlSetVelo([50, 50, 50, 50, 50], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
