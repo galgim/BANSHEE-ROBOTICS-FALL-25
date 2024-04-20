d6892b0f68ff6496cf9fad145be10c80ea05f77d
YeachanOkh <yeachanokh@gmail.com>
2024-04-19 17:26:33 -0700

    bvm push in test

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -313,9 +313,9 @@ def debug_bvm_pull_out():
     motor.simMotorRun(bvm_max_length_angle, [1, 2, 3, 4])
     time.sleep(0.4)
 
-    print("move 2 pitch wrist")
-    motor.simMotorRun([200], [4])  # Reset claw looking up
-    time.sleep(0.5)
+    # print("move 2 pitch wrist")
+    # motor.simMotorRun([200], [4])  # Reset claw looking up
+    # time.sleep(0.5)
 
     print("move 3 close grip")
     motor.simMotorRun([30], [0])  # Reset claw looking up
