135bdb57a5152ca43bb370695df8f38087e30858
YeachanOkh <yeachanokh@gmail.com>
2024-04-23 16:17:06 -0700

    bvm pull out finish

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -360,9 +360,9 @@ def debug_bvm_push_in():
     motor.simMotorRun([180, 62], [2, 3])
     time.sleep(0.5)
 
-    # print("move back to chamber")
-    # motor.simMotorRun([170, 62], [2, 3])
-    # time.sleep(0.5)
+    print("move back to chamber")
+    motor.simMotorRun([170, 62], [2, 3])
+    time.sleep(0.5)
 
     # print("move back to chamber")
     # motor.simMotorRun([160, 62], [2, 3])
