18a8d92af3b86dc3bf9a99d6dcdf883b82be931d
YeachanOkh <yeachanokh@gmail.com>
2024-04-23 20:06:15 -0700

    bvm push in test

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -390,7 +390,7 @@ def debug_bvm_push_in():
     motor.simMotorRun([110,270],[2,3])
     time.sleep(0.5)
     print("push battery insecure")
-    motor.simMotorRun([30, 223, 110, 210, 186], [0, 1, 2, 3, 4])
+    motor.simMotorRun([30, 223, 110, 130, 186], [0, 1, 2, 3, 4])
     time.sleep(0.5)
     print("set up move")
     motor.simMotorRun([95, 225, 270, 47, 272], [0, 1, 2, 3, 4])  # Reset claw looking up
