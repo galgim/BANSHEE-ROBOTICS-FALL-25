d866ff8c4e77d48b8070bfe7c26d0e7dacdd170d
YeachanOkh <yeachanokh@gmail.com>
2024-04-23 18:18:05 -0700

    bvm push in test

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -351,15 +351,14 @@ def debug_bvm_push_in():
     print("move back to chamber")
     motor.simMotorRun([223,185, 62], [1,2, 3])
     time.sleep(.5)
-    motor.simMotorRun([80,180],[3,4])
-    #[222, 168, 68, 268]
+    # print("adjust")
+    # motor.simMotorRun([80,180],[3,4])
+    # #[222, 168, 68, 268]
 
     gcs_push_in_angle = calculation.angle_Calc([180,-7,73], 0)
     print(gcs_push_in_angle)
     print("push in to chamber")
     motor.simMotorRun(gcs_push_in_angle, [1, 2, 3, 4])
-    time.sleep(10)
-    motor.simMotorRun([156],[2])
     time.sleep(.5)
   
     gcs_push_in_angle = calculation.angle_Calc([370,-7,73], 0)
