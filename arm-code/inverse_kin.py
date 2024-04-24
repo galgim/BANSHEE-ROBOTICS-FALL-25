5bea1a259972ade4aae792fb8bdd63b6104a6925
YeachanOkh <yeachanokh@gmail.com>
2024-04-23 18:11:40 -0700

    bvm push in test

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -342,7 +342,7 @@ def debug_bvm_push_in():
     #Push In Battery
     start_time = time.time()
     motor.dxlSetVelo([30, 30, 30, 36, 30], [0, 1, 2, 3, 4])  # ALWAYS SET SPEED BEFORE ANYTHING
-    time.sleep(0.1)s
+    time.sleep(0.1)
     motor.simMotorRun([223],[1])
     time.sleep(.1)
     
