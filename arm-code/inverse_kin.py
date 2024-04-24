af53db97a225f36c5fab9102a7b38cef7a4f0c70
YeachanOkh <yeachanokh@gmail.com>
2024-04-23 20:53:48 -0700

    bvm push in test

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -373,10 +373,11 @@ def debug_bvm_push_in():
     time.sleep(1)
 
     motor.simMotorRun([223],[1])
+
     # motor.simMotorRun([90],[4])
     # time.sleep(0.025)
-    # motor.simMotorRun([110,270],[2,3])
-    # time.sleep(0.5)
+    motor.simMotorRun([110,270],[2,3])
+    time.sleep(0.5)
     print("set up move")
     motor.simMotorRun([95, 225, 270, 47, 272], [0, 1, 2, 3, 4])  # Reset claw looking up
     time.sleep(2)
