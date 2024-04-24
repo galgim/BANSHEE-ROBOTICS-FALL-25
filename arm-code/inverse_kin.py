4955681a5cca2046b94f9255acaea7a8ba2f1c86
YeachanOkh <yeachanokh@gmail.com>
2024-04-24 15:11:42 -0700

    bvm push in test

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -390,6 +390,6 @@ if __name__ == "__main__":
     # time.sleep(5)
     # debug_gcs_push_in()
     
-    debug_bvm_pull_out()
-    time.sleep(5)
+    # debug_bvm_pull_out()
+    # time.sleep(5)
     debug_bvm_push_in()
