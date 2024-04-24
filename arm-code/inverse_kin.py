a4afe68ad8ef69a99dd78ce44490c90b145c24ce
YeachanOkh <yeachanokh@gmail.com>
2024-04-23 19:44:00 -0700

    bvm push in test

--- arm-code/inverse_kin.py
+++ arm-code/inverse_kin.py
@@ -359,7 +359,7 @@ def debug_bvm_push_in():
     time.sleep(5)
     #[223, 115, 191, 197] 1 2 3 4 
     print("adjust")
-    for i in range(170,280,10):
+    for i in range(190,280,10):
         print(i)
         initial_push_angle = calculation.angle_Calc([i,0,73], 0)
         print("push 4 slight")
