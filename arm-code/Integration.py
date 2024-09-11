25d3634b14a99baa3448b029c4cf3fd9b2c47bdf
galgim <claeekim@gmail.com>
2024-09-10 18:05:43 -0700

    hellochange

--- arm-code/Integration.py
+++ arm-code/Integration.py
@@ -56,13 +56,13 @@ def Hello():
     start_time = time.time()
     print("Hello")
     motor.dxlSetVelo([45, 55, 30, 30, 30], [0, 1, 2, 3, 4])
-    motor.simMotorRun([180, 50, 265], [3, 2, 4])
-    time.sleep(0.1)
-    motor.simMotorRun([180], [4])
+    # motor.simMotorRun([180, 50, 265], [3, 2, 4])
+    # time.sleep(0.1)
+    # motor.simMotorRun([180], [4])
     time.sleep(0.2)
     motor.simMotorRun([50], [0])
     time.sleep(0.9)
-    motor.simMotorRun([112],[0])
+    motor.simMotorRun([113],[0])
     time.sleep(0.9)
 
 
@@ -151,7 +151,7 @@ def Goodbye():
 def startsetup():
     print("set up move")
     motor.dxlSetVelo([30, 30, 30, 30, 30], [0, 1, 2, 3, 4])
-    motor.simMotorRun([112, 270, 130, 264, 270], [0, 1, 2, 3, 4])
+    motor.simMotorRun([113, 270, 130, 264, 270], [0, 1, 2, 3, 4])
     time.sleep(1)
 
 # Dictionary mapping commands to functions
