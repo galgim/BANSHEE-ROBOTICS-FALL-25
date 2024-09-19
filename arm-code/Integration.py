320cc859f2f5c3b1fe4d8b6f5250970bb5a427ad
galgim <claeekim@gmail.com>
2024-09-19 14:59:18 -0700

    clay

--- arm-code/Integration.py
+++ arm-code/Integration.py
@@ -70,7 +70,7 @@ def Yes():
     start_time = time.time()
     print("Yes")
     motor.dxlSetVelo([30, 30, 30, 30, 55], [0, 1, 2, 3, 4])
-    motor.simMotorRun([88,100], [2,3])
+    motor.simMotorRun([60,100], [2,3])
     time.sleep(1)
    
 
