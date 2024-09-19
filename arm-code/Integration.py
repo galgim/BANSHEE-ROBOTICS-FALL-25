0cf6452ae229dd673520cd945f3109ce26fb27af
galgim <claeekim@gmail.com>
2024-09-19 14:40:06 -0700

    clay

--- arm-code/Integration.py
+++ arm-code/Integration.py
@@ -146,7 +146,7 @@ def startsetup():
     print("set up move")
     motor.dxlSetVelo([30, 30, 30, 30, 30], [0, 1, 2, 3, 4])
     motor.simMotorRun([112, 270, 218, 264, 270], [0, 1, 2, 3, 4])
-    time.sleep(10)
+    time.sleep(1)
 
 # Dictionary mapping commands to functions
 Command_dict = {
