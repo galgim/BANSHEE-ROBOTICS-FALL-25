2a2e599a9afac1db3d23f6d708ed91a6dbf93b60
galgim <claeekim@gmail.com>
2024-10-22 15:46:58 -0700

    test

--- Banshee-ros/install/publisherID/share/publisherID/package.sh
+++ Banshee-ros/install/publisherID/share/publisherID/package.sh
@@ -80,7 +80,6 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/publisherID/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/publisherID/hook/ament_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/publisherID/share/publisherID/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
