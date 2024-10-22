d3bfb46cab261e97329cdb1c64ae8a95c801dbb6
YeachanOkh <yeachanokh@gmail.com>
2024-10-22 14:25:23 -0700

    Revert "stepper node"
    This reverts commit 9c4dfa457fc1101293f48d52e29910e0c91fc503.

--- Banshee-ros/build/publisherID/prefix_override/sitecustomize.py
+++ Banshee-ros/build/publisherID/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/banshee/BANSHEE-Mechatronics-SPR24/Banshee-ros/install/publisherID'
+    sys.prefix = sys.exec_prefix = '/home/ubuntu/BANSHEE-Mechatronics-SPR24/Banshee-ros/install/publisherID'
