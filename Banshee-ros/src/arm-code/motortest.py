50102c69b15d15f1ef52d9daa26262cf93854600
YeachanOkh <yeachanokh@gmail.com>
2024-10-22 18:38:13 -0700

    test

--- Banshee-ros/src/arm-code/motortest.py
+++ Banshee-ros/src/arm-code/motortest.py
@@ -69,7 +69,7 @@ if MY_DXL == 'X_SERIES' or MY_DXL == 'MX_SERIES':
     ADDR_PRESENT_POSITION       = 132
     DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
     DXL_MAXIMUM_POSITION_VALUE  = 4095      # Refer to the Maximum Position Limit of product eManual
-    BAUDRATE                    = 57600
+    BAUDRATE                    = 1000000
 elif MY_DXL == 'PRO_SERIES':
     ADDR_TORQUE_ENABLE          = 562       # Control table address is different in DYNAMIXEL model
     ADDR_GOAL_POSITION          = 596
