ea4e004633eed5c501292f2201193ab7b8ee87b2
galgim <claeekim@gmail.com>
2024-10-29 16:38:49 -0700

    clay

--- Banshee-ros/src/arm-code/Integration.py
+++ Banshee-ros/src/arm-code/Integration.py
@@ -141,7 +141,7 @@ def Goodbye():
 def startsetup():
     print("set up move")
     motor.dxlSetVelo([30, 10, 10, 30, 10], [0, 1, 2, 3, 4])
-    motor.simMotorRun([108], [0])
+    motor.simMotorRun([110], [0])
     # motor.simMotorRun([112, 132, 257, 144], [0, 2, 3, 4])
     # motor.simMotorRun([112, 270, 218, 264, 270], [0, 1, 2, 3, 4])
     time.sleep(10)
