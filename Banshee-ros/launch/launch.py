dd7e26328b30513025daaaf6025ae9a96fb116f5
Michael Rosales <rosalesmichael502@gmail.com>
2024-10-31 18:10:51 -0700

    test

---
+++ Banshee-ros/launch/launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='stepper_package',  
+            executable='stepper_motor_node',
+            name='stepper_motor',
+            parameters=[
+                {"done_signal_topic": "/stepper/done"} 
+            ]
+        ),
+        Node(
+            package='integration_package',  
+            executable='integration_node',  
+            name='integration',
+            parameters=[
+                {"start_signal_topic": "/stepper/done"}  
+            ]
+        ),
+
+    ])
