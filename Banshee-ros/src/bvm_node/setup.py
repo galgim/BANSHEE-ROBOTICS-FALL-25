bc3267b1eeb2b0eda613f6943ec8017838c6b388
Cjoshua28 <tinyjoshua28@gmail.com>
2024-11-07 14:56:35 -0800

    test

--- Banshee-ros/src/bvm_node/setup.py
+++ Banshee-ros/src/bvm_node/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ubuntu',
-    maintainer_email='tinyjoshua28@gmail.com',
+    maintainer_email='joshuacarrasco428@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
