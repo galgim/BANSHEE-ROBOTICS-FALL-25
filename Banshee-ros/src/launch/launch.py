3abd5273356a3cadd1e5e1fc2fe0e67657e14967
Cjoshua28 <tinyjoshua28@gmail.com>
2024-11-07 14:45:27 -0800

    test

--- Banshee-ros/src/launch/launch.py
+++ Banshee-ros/src/launch/launch.py
@@ -11,12 +11,4 @@ def generate_launch_description():
             #     {"start_signal_topic": "/stepper/done"}  
             # ]
         ),
-        # Node(
-        #     package='stepper_package',  
-        #     executable='stepper_motor_node',
-        #     name='stepper_motor',
-        #     # parameters=[
-        #     #     {"done_signal_topic": "/stepper/done"} 
-        #     # ]
-        # ),
     ])
