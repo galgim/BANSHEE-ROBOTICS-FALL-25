b4cf73256316eec8fe2d7655b7ff7a1388a99c43
Cjoshua28 <tinyjoshua28@gmail.com>
2024-11-12 16:15:41 -0800

    test

--- Banshee-ros/src/stepper_control/stepper_control/stepper_node.py
+++ Banshee-ros/src/stepper_control/stepper_control/stepper_node.py
@@ -47,7 +47,7 @@ class StepperMotorNode(Node):
             # Max steps in CCW 4050
             GPIO.output(DIR, CCW)
             sleep(1)
-            for _ in range(300):
+            for _ in range(121):
                 GPIO.output(STEP, GPIO.HIGH)
                 sleep(0.0007)
                 GPIO.output(STEP, GPIO.LOW)
