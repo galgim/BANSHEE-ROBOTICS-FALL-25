8a28fc660ea80b6e4897cc042655a220931a9f8e
galgim <claeekim@gmail.com>
2024-11-14 14:41:13 -0800

    clay

--- Banshee-ros/src/integration/integration/integration_node.py
+++ Banshee-ros/src/integration/integration/integration_node.py
@@ -39,162 +39,42 @@ motor.portInitialization(PORT_NUM, ALL_IDs)
 # Calculate the angle for the max length reaching out in the x position
 max_length_angle = calculation.angle_Calc([375, 0, 73], 0)
 
+# define
+TOP_BVM = 0
+BOT_BVM = 1
+DRONE_BAT = 2
 
+def pull_out(dev):
+    if dev == 0:
+        motor.dxlSetVelo([30, 30, 30, 30, 10], [0, 1, 2, 3, 4])
+        # motor.simMotorRun([x,x,x,x,x],[0,1,2,3,4])
+        time.sleep(1)
+    elif dev == 1:
+        motor.dxlSetVelo([30, 30, 30, 30, 10], [0, 1, 2, 3, 4])
+        # motor.simMotorRun([x,x,x,x,x],[0,1,2,3,4])
+        time.sleep(1)
+    elif dev == 2:
+        motor.dxlSetVelo([30, 30, 30, 30, 10], [0, 1, 2, 3, 4])
+        # motor.simMotorRun([x,x,x,x,x],[0,1,2,3,4])
+        time.sleep(1)
+    else:
+        print("INVALID")
 
-
-# Check movement of motors
-def checkMovement(ids):
-  motorStatus = [0] * len(ids)
-  finished = [1] * len(ids)
-  firstPosition = 0
-  secondPosition = 0
-  while True:
-      for id in ids:
-          firstPosition = motor.ReadMotorData(id, ADDR_PRESENT_POSITION)
-          time.sleep(0.1)
-          secondPosition = motor.ReadMotorData(id, ADDR_PRESENT_POSITION)
-          if abs(firstPosition - secondPosition) < 5:
-              motorStatus[id] = 1
-      if motorStatus == finished:
-          print("finished")
-          break
-
-
-
-
-# Define various hand movements
-def Hello():
-  start_time = time.time()
-  print("Hello")
-  motor.dxlSetVelo([45, 55, 30, 30, 30], [0, 1, 2, 3, 4])
-  motor.simMotorRun([180, 30, 265], [3, 2, 4])
-  time.sleep(0.1)
-  motor.simMotorRun([180], [4])
-  time.sleep(0.2)
-  motor.simMotorRun([45], [0])
-  time.sleep(0.9)
-  motor.simMotorRun([112],[0])
-  time.sleep(0.9)
-
-
-
-
-def Yes():
-  start_time = time.time()
-  print("Yes")
-  motor.dxlSetVelo([30, 30, 40, 40, 55], [0, 1, 2, 3, 4])
-  motor.simMotorRun([60,100], [2,3])
-  time.sleep(10)
-
-
-
-
-def Fistbump():
-  start_time = time.time()
-  print("fistbump")
-  motor.dxlSetVelo([30, 30, 30, 30, 55], [0, 1, 2, 3, 4])
-  motor.simMotorRun([200, 80, 180], [3, 2, 4])
-  motor.dxlSetVelo([55, 55, 55], [2, 3, 4])
-  time.sleep(0.1)
-  motor.simMotorRun([60, 180, 172], [2, 3, 4])
-  time.sleep(0.3)
-  motor.simMotorRun([200, 80, 180], [3, 2, 4])
-
-
-
-
-def Highfive():
-  start_time = time.time()
-  print("Highfive")
-  motor.dxlSetVelo([30, 30, 30, 30, 65], [0, 1, 2, 3, 4])
-  motor.simMotorRun([265, 160, 40], [4, 3, 2])
-  motor.dxlSetVelo([30, 30, 50, 50, 50], [0, 1, 2, 3, 4])
-  motor.simMotorRun([20, 135, 245], [2, 3, 4])
-  time.sleep(0.2)
-  motor.simMotorRun([40, 160, 265], [2, 3, 4])
-
-
-
-
-def Handshake():
-  start_time = time.time()
-  print("Handshake")
-  motor.dxlSetVelo([40, 40, 40, 40, 40], [0, 1, 2, 3, 4])
-  motor.simMotorRun([185, 65, 0, 150], [3, 2, 0, 4])
-  time.sleep(0.5)
-  motor.dxlSetVelo([55, 55, 55, 55, 55], [0, 1, 2, 3, 4])
-  motor.simMotorRun([155, 175], [4, 3])
-  time.sleep(0.1)
-  motor.simMotorRun([195, 205], [4, 3])
-  time.sleep(0.1)
-  motor.simMotorRun([155, 175], [4, 3])
-  time.sleep(0.1)
-  motor.simMotorRun([195, 205], [4, 3])
-
-
-def Thankyou():
-  start_time = time.time()
-  print("Thank you")
-  motor.dxlSetVelo([60, 60, 60, 60, 60], [0, 1, 2, 3, 4])
-  motor.simMotorRun([130,180],[2,3])
-  time.sleep(0.01)
-  motor.simMotorRun([180],[4])
-  time.sleep(0.01)
-  motor.simMotorRun([230],[3])
-  time.sleep(0.01)
-  motor.simMotorRun([180],[3])
-  time.sleep(0.01)
-  motor.simMotorRun([230],[3])
-  time.sleep(0.01)
-  motor.simMotorRun([180],[3])
-
-
-def No():
-  start_time = time.time()
-  print("No")
-  motor.dxlSetVelo([30, 30, 30, 30, 55], [0, 1, 2, 3, 4])
-  motor.simMotorRun([220, 85, 265], [3, 2, 4])
-
-
-
-
-def Goodbye():
-  start_time = time.time()
-  print("Goodbye")
-  motor.dxlSetVelo([30, 30, 30, 30, 55], [0, 1, 2, 3, 4])
-  motor.simMotorRun([220, 85, 265], [3, 2, 4])
-
-
-
-
-
-
-
-
-# Setup initial motor positions
-def startsetup():
-  print("set up move")
-  motor.dxlSetVelo([30, 30, 30, 30, 30], [0, 1, 2, 3, 4])
-  motor.simMotorRun([112, 180, 180, 180, 180], [0, 1, 2, 3, 4])
-  time.sleep(1)
-
-
-
-
-# Dictionary mapping commands to functions
-Command_dict = {
-  "hello": Hello,
-  "no": No,
-  "thankyou": Thankyou,
-  "handshake": Handshake,
-  "highfive": Highfive,
-  "goodbye": Goodbye,
-  "yes": Yes,
-  "fistbump": Fistbump,
-  "setup": startsetup,
-}
-
-
+def push_in(dev):
+    if dev == 0:
+        motor.dxlSetVelo([30, 30, 30, 30, 10], [0, 1, 2, 3, 4])
+        # motor.simMotorRun([x,x,x,x,x],[0,1,2,3,4])
+        time.sleep(1)
+    elif dev == 1:
+        motor.dxlSetVelo([30, 30, 30, 30, 10], [0, 1, 2, 3, 4])
+        # motor.simMotorRun([x,x,x,x,x],[0,1,2,3,4])
+        time.sleep(1)
+    elif dev == 2:
+        motor.dxlSetVelo([30, 30, 30, 30, 10], [0, 1, 2, 3, 4])
+        # motor.simMotorRun([x,x,x,x,x],[0,1,2,3,4])
+        time.sleep(1)
+    else:
+        print("INVALID")
 
 
 class IntegrationNode(Node):
@@ -223,22 +103,21 @@ class IntegrationNode(Node):
 
     def run(self):
       self.get_logger().info("Waiting for 'done' signal from Stepper Node...")  
-      
+
       if self.start_signal_received and self.mode == 0:
         # Proceed to command execution after receiving 'done' signal
-        Command_dict["hello"]()
+        pull_out(self.mode)
         self.mode = 1
         self.start_signal_received = False
         self.armFinished.publish(True)
 
       elif self.start_signal_received and self.mode == 1:
         # Proceed to command execution after receiving 'done' signal
-        Command_dict["place"]()
+        push_in(self.mode)
         self.mode = 0
         self.start_signal_received = False
         self.armFinished.publish(True)
-         
-
+        
 
 def main(args=None):
     rclpy.init(args=args)
