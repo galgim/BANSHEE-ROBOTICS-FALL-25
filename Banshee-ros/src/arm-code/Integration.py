94035bd54df800b0b1c3d5d4d59081bc1aa3ab81
YeachanOkh <yeachanokh@gmail.com>
2024-12-03 21:56:28 -0800

    test

--- Banshee-ros/src/arm-code/Integration.py
+++ Banshee-ros/src/arm-code/Integration.py
@@ -60,7 +60,7 @@ def Drone_grab():
 def Push_low():    
     start_time = time.time()
     print("Push in low sequence")
-    motor.dxlSetVelo([10, 10, 20], [2, 3, 4])
+    motor.dxlSetVelo([10, 10, 15], [2, 3, 4])
     # max_length_angle = calculation.angle_Calc([190, 0, 73], 0)
     # motor.simMotorRun(max_length_angle, [1, 2, 3, 4])
     # for i in range(270,370,10):
@@ -69,8 +69,8 @@ def Push_low():
     #     print("move 4 pull away slight")
     #     motor.simMotorRun(initial_pull_out_angle, [1, 2, 3, 4])
     #     time.sleep(3)
-
-    motor.simMotorRun([132, 343, 70],[2, 3, 4]) # guesstimate
+    print("place chamber")
+    motor.simMotorRun([132, 310, 70],[2, 3, 4]) # guesstimate
     time.sleep(5)
     motor.dxlSetVelo([10, 8, 5], [2, 3, 4])
     motor.simMotorRun([110, 330, 60],[2, 3, 4])
