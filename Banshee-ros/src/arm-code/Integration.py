8004b2a22a914bf8d7de6b3905efa8755bf41012
YeachanOkh <yeachanokh@gmail.com>
2024-12-03 21:14:13 -0800

    test

--- Banshee-ros/src/arm-code/Integration.py
+++ Banshee-ros/src/arm-code/Integration.py
@@ -61,6 +61,7 @@ def Push_low():
     start_time = time.time()
     print("Push in low sequence")
     motor.dxlSetVelo([10, 10, 20], [2, 3, 4])
+    motor.dxlPresPos([0, 1, 2, 3, 4])
     # max_length_angle = calculation.angle_Calc([190, 0, 73], 0)
     # motor.simMotorRun(max_length_angle, [1, 2, 3, 4])
     for i in range(375,280,-10):
@@ -69,7 +70,6 @@ def Push_low():
         print("move 4 pull away slight")
         motor.simMotorRun(initial_pull_out_angle, [1, 2, 3, 4])
         time.sleep(3)
-        motor.dxlPresPos([0, 1, 2, 3, 4])
 
     # motor.simMotorRun([132, 343, 70],[2, 3, 4]) # guesstimate
     # time.sleep(5)
