7d30a44f51e210f79e13d30c29cd227e66717085
galgim <claeekim@gmail.com>
2024-12-05 14:59:41 -0800

    clay

--- Banshee-ros/src/arm-code/Integration.py
+++ Banshee-ros/src/arm-code/Integration.py
@@ -74,7 +74,7 @@ def Pull_low():
     print("remove chamber")
     motor.simMotorRun([80, 248, 95],[2, 3, 4])
     Close()
-    motor.dxlSetVelo([30, 40, 30], [2, 3, 4])
+    motor.dxlSetVelo([20, 40, 20], [2, 3, 4])
     motor.simMotorRun([132, 343, 70],[2, 3, 4])
 
 
