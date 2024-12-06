317689e360c0051a5746a6ef31993e5eaad90999
galgim <claeekim@gmail.com>
2024-12-05 17:33:39 -0800

    clay

--- Banshee-ros/src/arm-code/Integration.py
+++ Banshee-ros/src/arm-code/Integration.py
@@ -80,7 +80,7 @@ def Pull_high():
     motor.simMotorRun([60, 180, 144],[2, 3, 4])  # move arm to chamber position
     Close()                                     # grab battery
     motor.dxlSetVelo([12, 30, 20], [2, 3, 4])   # set pull out velocity
-    motor.simMotorRun([130, 310, 90],[2, 3, 4]) # pull battery out
+    motor.simMotorRun([130, 310, 85],[2, 3, 4]) # pull battery out
 
 
 # Close Claw
