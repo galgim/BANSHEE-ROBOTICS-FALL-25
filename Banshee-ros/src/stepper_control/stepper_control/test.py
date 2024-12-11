9067fbe9028f3305a0cd04fa9277bbe947406533
YeachanOkh <yeachanokh@gmail.com>
2024-12-10 17:01:08 -0800

    Test

--- Banshee-ros/src/stepper_control/stepper_control/test.py
+++ Banshee-ros/src/stepper_control/stepper_control/test.py
@@ -1,31 +1,20 @@
+
 import time
-
-from pinpong.board import Board,Pin
-
-#Board("uno").begin() #Initialization, select board type (uno, microbit, RPi,) and port number. If the port number is not entered, it will be automatically recognized
-
-#Board("leonardo","COM5").begin() #Specify the port initialization under Windows
-
-Board("uno","/dev/ttyS1").begin() #Specify the port initialization under Linux
-
-#Board("uno","/dev/cu.usbmodem14101").begin() #Specify the port initialization under MacOS
-
-led = Pin(Pin.D13, Pin.OUT) #Pin initialization as level output
-
+from pinpong.board import Board, Pin
+ 
+Board("uno").begin()  # Initialization, select board type (uno, microbit, RPi, handpy) and port number. If no port number is entered, automatic detection will be performed
+#Board("uno", "COM36").begin()  # Initialization with specified port on Windows
+Board("uno", "/dev/ttyACM0").begin()  # Initialization with specified port on Linux
+#Board("uno", "/dev/cu.usbmodem14101").begin()  # Initialization with specified port on Mac
+led = Pin(Pin.D13, Pin.OUT)  # Initialize the pin for digital output
+ 
 while True:
-
-    #led.value(1) #Output high level method 1
-
-    led.write_digital(1) #Output high level method 2
-
-    print("1") #Terminal printing information.
-
-    time.sleep(1) #Wait for 1 second and keep the state
-
-    #led.value(0) #Output Low Level Method 1
-
-    led.write_digital(0) #Output Low Level Method 1
-
-    print("0") #Terminal printing information.
-
-    time.sleep(1) #Wait for 1 second and keep the state
+  # led.value(1)  # Output high level Method 1
+  led.write_digital(1)  # Output high level Method 2
+  print("1")  # Print information in the terminal
+  time.sleep(1)  # Wait for 1 second to maintain the state
+ 
+  # led.value(0)  # Output low level Method 1
+  led.write_digital(0)  # Output low level Method 2
+  print("0")  # Print information in the terminal
+  time.sleep(1)  # Wait for 1 second to maintain the state
