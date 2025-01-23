cf0931757925de9b47fcaedfc7d3e37abe85af1e
galgim <claeekim@gmail.com>
2025-01-22 16:29:43 -0800

    clay

---
+++ Banshee-ros/src/Mavlink/test.py
@@ -0,0 +1,15 @@
+import time
+import sys
+from pymavlink import mavutil
+
+master = mavutil.mavlink_connection("/dev/ttyAMA0", baud=115200)
+master.wait_heartbeat()
+print("HEARTBEAT_DONE")
+ 
+master.param_fetch_all()
+ 
+while True:
+            m = master.recv_match(type = "PARAM_VALUES", blocking = True, timeout = 1)
+            print(m)
+            if m is None:
+                        break
