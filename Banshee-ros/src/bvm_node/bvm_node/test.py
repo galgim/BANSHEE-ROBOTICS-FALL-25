096bbe7f8cc754374e8b452a4cec0099ca3b8d89
Cjoshua28 <tinyjoshua28@gmail.com>
2025-01-22 17:43:15 -0800

    test

--- Banshee-ros/src/bvm_node/bvm_node/test.py
+++ Banshee-ros/src/bvm_node/bvm_node/test.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 def espSend(response):
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     changetobyte = response.encode(encoding="utf-8") 
     ser.write(changetobyte)
     print("send code to arduino")
