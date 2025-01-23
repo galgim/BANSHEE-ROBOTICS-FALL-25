240c7f8401c2bf285bbf6108bdd2c1934b0dfdd2
galgim <claeekim@gmail.com>
2025-01-22 17:00:01 -0800

    clay

--- Banshee-ros/src/arm-code/Integration.py
+++ Banshee-ros/src/arm-code/Integration.py
@@ -48,7 +48,7 @@ def Push_low():
     print("place chamber")
     motor.simMotorRun([132, 343, 70],[2, 3, 4]) # move arm to first position
     motor.dxlSetVelo([15, 28, 10], [2, 3, 4])   # set final velocity
-    motor.simMotorRun([75, 228, 95],[2, 3, 4])  # move arm w/ battery to chamber
+    motor.simMotorRun([80, 248, 95],[2, 3, 4])  # move arm w/ battery to chamber
     Open()                                      # let go of battery
 
 def Pull_low():    
