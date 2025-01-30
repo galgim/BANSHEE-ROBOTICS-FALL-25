8f859e9790e6b43911eb6323fa0729f7f8c26f91
galgim <claeekim@gmail.com>
2025-01-30 14:03:01 -0800

    clay

--- Banshee-ros/src/arm-code/Integration.py
+++ Banshee-ros/src/arm-code/Integration.py
@@ -85,19 +85,20 @@ def Drone_push():
     print("push drone bat")
     motor.simMotorRun([45], [1])                                # turn around
     motor.simMotorRun([154, 334, 90],[2, 3, 4])                 # move arm to first position
-    motor.dxlSetVelo([17, 27, 8], [2, 3, 4])                    # set final velocity
+    motor.dxlSetVelo([17, 27, 8], [2, 3, 4])                    # set push in velocity
     motor.simMotorRun([95, 243, 112],[2, 3, 4])                 # move arm to chamber position
     Open()                                                      # push battery
 
 def Drone_pull():
     start_time = time.time()
     print("Drone pull sequence")
-    motor.dxlSetVelo([30, 50, 30, 30, 30], [0, 1, 2, 3, 4])      # set initial velocity
-    print("remove drone bat")                                                  
+    motor.dxlSetVelo([30, 50, 30, 30, 30], [0, 1, 2, 3, 4])     # set initial velocity
+    print("remove drone bat")
+    motor.simMotorRun([45], [1])                                # turn around                  
     motor.simMotorRun([95, 243, 112],[2, 3, 4])                 # move to battery position
     Close()  
-    motor.dxlSetVelo([17, 27, 8], [2, 3, 4])                   # set pull out velocity
-    motor.simMotorRun([154, 334, 90],[2, 3, 4])                      # pull battery out
+    motor.dxlSetVelo([17, 27, 8], [2, 3, 4])                    # set pull out velocity
+    motor.simMotorRun([154, 334, 90],[2, 3, 4])                 # pull battery out
 
 ####################
 
