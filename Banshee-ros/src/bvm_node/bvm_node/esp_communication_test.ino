312266336df54de921667fe9dd9bc032e257cfa6
YeachanOkh <yeachanokh@gmail.com>
2025-02-03 14:36:40 -0800

    test

--- Banshee-ros/src/bvm_node/bvm_node/esp_communication_test.ino
+++ Banshee-ros/src/bvm_node/bvm_node/esp_communication_test.ino
@@ -1,7 +1,6 @@
 /*
-
 void setup() {
-  // Start the serial communication at 9600 baud rate
+  // Start the serial communication at 115200 baud rate
   Serial.begin(115200);
 }
 
@@ -10,12 +9,13 @@ void loop() {
   if (Serial.available() > 0) {
     // Read the incoming byte:
     String incomingData = Serial.readStringUntil('\n');  // Read until newline
-    Serial.print("Received: ");
-    Serial.println(incomingData);  // Print received data
+//    Serial.print("Received: ");
+//    Serial.println(incomingData);  // Print received data
 
     // Respond back to the sender (e.g., your Python script)
-    String response = "Message received: " + incomingData;
-    Serial.println(response);  // Send a response back to the serial
+    byte response[8];
+    response[0] = 1.2;
+    Serial.write(response, 8);  // Send a response back to the serial
   }
 
   delay(1000);
