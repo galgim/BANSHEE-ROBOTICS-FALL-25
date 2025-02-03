600d11e438c525f4a2da3f77c43575028e601502
Cjoshua28 <tinyjoshua28@gmail.com>
2025-02-03 13:42:42 -0800

    test

--- Banshee-ros/src/bvm_node/bvm_node/bvm_code.py
+++ Banshee-ros/src/bvm_node/bvm_node/bvm_code.py
@@ -67,20 +67,6 @@ class BVMNode(Node):
             print(line)
             ser.reset_input_buffer()
         time.sleep(1)
-
-    def espReceive(response):
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        changetobyte = response.encode(encoding="utf-8") 
-        ser.write(changetobyte)
-        print("send code to arduino")
-        # Add a delay to ensure all responses are sent by the Arduino
-        time.sleep(1)
-        
-        while ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
-            ser.reset_input_buffer()
-        time.sleep(1)
     
     def bvmLogic(self):
         if len(self.DroneMarkers) > 0:
--- Banshee-ros/src/bvm_node/bvm_node/test.py
+++ Banshee-ros/src/bvm_node/bvm_node/test.py
@@ -1,23 +1,10 @@
 import serial
-import time
 
-def espSend(response):
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    changetobyte = response.encode(encoding="utf-8") 
-    ser.write(changetobyte)
-    print("send code to arduino")
-    # Add a delay to ensure all responses are sent by the Arduino
-    time.sleep(1)
-    
-    while ser.in_waiting > 0:
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        ser.reset_input_buffer()
-    time.sleep(1)
+# Change '/dev/ttyUSB0' to the correct port (COMx on Windows)
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 
-def main(args=None):
-    espSend("hello")
-
-
-if __name__ == '__main__':
-    main()
+while True:
+    ser.write(b'Hello ESP32\n')  # Send data
+    response = ser.readline().decode('utf-8').strip()  # Read response
+    if response:
+        print("Received from ESP32:", response)
