fd0e7ca84b6f61d75227a82c530190571f0bb4cd
YeachanOkh <yeachanokh@gmail.com>
2025-02-11 20:03:40 -0800

    Test

--- Banshee-ros/src/stepper_control/stepper_control/stepper_node.py
+++ Banshee-ros/src/stepper_control/stepper_control/stepper_node.py
@@ -1,16 +1,139 @@
+# import rclpy
+# from rclpy.node import Node
+# from std_msgs.msg import Bool, Float32, Int8
+# # import RPi.GPIO as GPIO
+# from Arduino import Arduino
+# from time import sleep
+
+# # Pin Definitions
+# DIR = 10  # Direction Pin (Dir+)
+# STEP = 8  # Step Pin (Pul+)
+
+# CW = "HIGH"   # Clockwise Rotation
+# CCW = "LOW"  # Counter Clockwise Rotation
+
+# # Set Positions in BTP
+# COLUMN1 = 500
+# COLUMN2 = 1481
+# COLUMN3 = 2492
+# COLUMN4 = 3510
+
+
+# class StepperMotorNode(Node):
+#     def __init__(self):
+#         super().__init__('stepper_motor_node')
+        
+#         # GPIO.setmode(GPIO.BOARD)
+#         # GPIO.setup(DIR, GPIO.OUT)
+#         # GPIO.setup(STEP, GPIO.OUT)
+
+#         self.board = Arduino(115200, port="/dev/ttyS1")
+#         self.board.pinMode(DIR, "OUTPUT")
+#         self.board.pinMode(STEP, "OUTPUT")
+        
+#         self.stepCoefficient = 500/159.5
+#         self.position = 0
+        
+#         # ROS2 Publisher and Subscribers
+#         self.initialSubscription = self.create_subscription(
+#         Int8, 'arucoID', self.initialSubscriber, 10)
+
+#         self.distanceSubscription = self.create_subscription(
+#         Float32, 'DestinationFalse', self.distanceSubscriber, 10)
+
+#         self.done_publisher = self.create_publisher(Bool, 'stepperDone', 10)
+
+#         self.get_logger().info('Stepper Motor Node has been started and is ready for commands.')
+
+#     def initialSubscriber(self, msg):
+#         arucoID = msg.data
+#         self.get_logger().info(f"Received Aruco ID: {arucoID}")
+
+#         aruco_to_column = {
+#                 0: COLUMN1, 4: COLUMN1,
+#                 1: COLUMN2, 5: COLUMN2,
+#                 2: COLUMN3, 6: COLUMN3,
+#                 3: COLUMN4, 7: COLUMN4
+#             }
+#         self.run_motor_cycle(aruco_to_column.get(arucoID, None))
+
+#     def distanceSubscriber(self, msg):
+#         distance = msg.data
+#         self.get_logger().info(f"Received distance: {distance}")
+#         addedSteps = distance * self.stepCoefficient
+#         self.run_motor_cycle(self.position + addedSteps)
+
+#     def run_motor_cycle(self, newPosition):
+#         try:
+#             if newPosition is not None:
+#                 self.get_logger().info(str(newPosition) + " is new position")
+
+#                 steps = newPosition - self.position
+
+#                 # Max steps in CW 4050
+#                 if self.position + steps > 4050 or self.position + steps < 0:
+#                     self.get_logger().warn("Distance out of range. Movement skipped.")
+#                     return
+
+#                 if steps > 0:
+#                     self.board.digitalWrite(DIR, CW)
+#                     # GPIO.output(DIR, CW)
+#                 else:
+#                     self.board.digitalWrite(DIR, CCW)
+#                     # GPIO.output(DIR, CCW)
+#                 for _ in range(abs(round(steps))):   
+#                     self.board.digitalWrite(STEP, "HIGH")                
+#                     # GPIO.output(STEP, GPIO.HIGH)
+#                     sleep(0.002) 
+#                     self.board.digitalWrite(STEP, "LOW")
+#                     # GPIO.output(STEP, GPIO.LOW)
+#                     sleep(0.002)
+                
+#                 self.position = self.position + steps
+
+#                 sleep(1)
+#                 # Publish cycle complete signal
+#                 self.get_logger().info('Cycle complete, publishing signal to camera')
+#                 cycle_complete_msg = Bool()
+#                 cycle_complete_msg.data = True
+#                 self.done_publisher.publish(cycle_complete_msg)
+#         except KeyboardInterrupt:
+#             self.cleanup()
+
+#     def cleanup(self):
+#         self.get_logger().info('Cleaning up...')
+#         # GPIO.cleanup()
+
+
+# def main(args=None):
+#     rclpy.init(args=args)
+    
+#     stepper_motor_node = StepperMotorNode()
+    
+#     try:
+#         rclpy.spin(stepper_motor_node)
+#     except KeyboardInterrupt:
+#         stepper_motor_node.cleanup()
+#     finally:
+#         stepper_motor_node.destroy_node()
+#         rclpy.shutdown()
+
+
+# if __name__ == '__main__':
+#     main()
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool, Float32, Int8
 # import RPi.GPIO as GPIO
-from Arduino import Arduino
-from time import sleep
-
+from time import sleep 
+from pinpong.board import Board, Pin
+Board("leonardo", "/dev/ttyACM0").begin()  # Initialization with specified port on Linux
 # Pin Definitions
-DIR = 10  # Direction Pin (Dir+)
-STEP = 8  # Step Pin (Pul+)
+DIR = Pin.D10  # Direction Pin (Dir+)
+STEP = Pin.D8  # Step Pin (Pul+)
 
-CW = "HIGH"   # Clockwise Rotation
-CCW = "LOW"  # Counter Clockwise Rotation
+CW = 1   # Clockwise Rotation
+CCW = 0  # Counter Clockwise Rotation
 
 # Set Positions in BTP
 COLUMN1 = 500
@@ -22,15 +145,9 @@ COLUMN4 = 3510
 class StepperMotorNode(Node):
     def __init__(self):
         super().__init__('stepper_motor_node')
-        
-        # GPIO.setmode(GPIO.BOARD)
-        # GPIO.setup(DIR, GPIO.OUT)
-        # GPIO.setup(STEP, GPIO.OUT)
+        self.rotation = Pin(DIR, Pin.OUT)  # Initialize the pin for digital output
+        self.movement = Pin(STEP, Pin.OUT)
 
-        self.board = Arduino(115200, port="/dev/ttyS1")
-        self.board.pinMode(DIR, "OUTPUT")
-        self.board.pinMode(STEP, "OUTPUT")
-        
         self.stepCoefficient = 500/159.5
         self.position = 0
         
@@ -76,16 +193,16 @@ class StepperMotorNode(Node):
                     return
 
                 if steps > 0:
-                    self.board.digitalWrite(DIR, CW)
+                    self.rotation.write_digital(CW)
                     # GPIO.output(DIR, CW)
                 else:
-                    self.board.digitalWrite(DIR, CCW)
+                    self.rotation.write_digital(CCW)
                     # GPIO.output(DIR, CCW)
                 for _ in range(abs(round(steps))):   
-                    self.board.digitalWrite(STEP, "HIGH")                
+                    self.movement.write_digital(1)         
                     # GPIO.output(STEP, GPIO.HIGH)
                     sleep(0.002) 
-                    self.board.digitalWrite(STEP, "LOW")
+                    self.movement.write_digital(0)
                     # GPIO.output(STEP, GPIO.LOW)
                     sleep(0.002)
                 
