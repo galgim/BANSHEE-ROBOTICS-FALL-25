9081f5a30f0bb6bdb2792bf1aebae39ac7f9fcd5
Cjoshua28 <tinyjoshua28@gmail.com>
2025-02-17 01:40:22 -0800

    bvm test

--- Banshee-ros/src/bvm_node/bvm_node/bvm_code.py
+++ Banshee-ros/src/bvm_node/bvm_node/bvm_code.py
@@ -1,128 +1,11 @@
-# import rclpy
-# from rclpy.node import Node
-# from std_msgs.msg import Bool, Int8
-# import serial
-# import time
-# import struct
-# #drone battery 4-5
-# #battery columns 0-3
-# #we get drone landed done from gcs node (mode 1)
-# #voltage from esp and find lowest aruco with no voltage
-# #find max voltage battery lowest aruco
-# #back to drone aruco we previously
-# class BVMNode(Node):
-#     def __init__(self):
-#         super().__init__("BVM_Node")
-
-#         # Mode 0 = standby
-#         # Mode 1 = Begin battery transfer from drone to BVM
-#         # Mode 2 = Begin battery transfer from BVM to drone
-#         # Mode 3 = Done with Battery, set every value back to default standby value (If another battery, go back to mode 1)
-#         self.mode = 0
-#         self.done = 0 # flag for each mode
-#         self.checkModeComplete = False
-#         self.DroneMarkers = [4, 5]
-#         self.batteryChamber = None
-#         self.ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-
-
-#         self.arucoPublisher = self.create_publisher(
-#         Int8, 'arucoID', 10)
-#         self.get_logger().info("ArucoID Publisher started")
-
-#         self.armSubscriber = self.create_subscription(
-#         Bool, 'modeComplete', self.modeComplete, 10)
-
-#         # self.arucoIDPublisher()
-
-#         # Uncomment line and delete arucoID() once finished with GCS node
-#         self.run_timer = self.create_timer(0.1, self.bvmLogic)
-    
-#     def arucoIDPublisher(self):
-#         msg = Int8()
-#         msg.data = int(input("Input Battery Chamber: "))
-#         self.arucoPublisher.publish(msg)
-#         self.get_logger().info('Sent marker: "%s"' % msg.data)
-
-#         self.arucoIDPublisher()
-    
-#     def modeComplete(self, msg):
-#         if msg.data:
-#             self.mode += 1
-#             self.done = 0
-    
-#     def batteryAmount(self, msg):
-#         self.batteries = msg.data
-
-#     def structUnpack(self, type, data):
-#         try:
-#             return [round(v,2) for v in struct.unpack(type, data)]
-#         except:
-#             self.get_logger().info("Incomplete data received")
-
-#     def espRead(self):
-#         if self.ser.in_waiting > 0:
-#             tag = self.ser.readline().decode('utf-8').strip()
-#             # Examples of reading from ESP
-#             if tag == "Voltage":
-#                 raw_data = self.ser.read(32)
-#                 values = self.structUnpack('8f', raw_data)
-#             elif tag == "OtherInfo":
-#                 raw_data = self.ser.read(12)
-#                 values = self.structUnpack('3i', raw_data)
-#             else:
-#                 return
-#             self.get_logger().info("Tag: " + tag)
-#             self.get_logger().info(f"{values}")
-
-#     def espSend(self, tag, data):
-#         if isinstance(data, list):
-#             self.ser.write((f"{tag}").encode('utf-8'))
-#             self.ser.write(bytearray(data), len(data))
-
-
-        
-#     def bvmLogic(self):
-#         if len(self.DroneMarkers) > 0:
-#             if self.mode == 0:
-#                 self.espRead()
-#             elif self.mode == 1 and self.done == 0:
-                
-
-
-
-#                 # Get BVM aruco ID needed
-#                 arucoID = Int8() 
-#                 arucoID.data = None # Figure out how to get it from esp
-#                 self.arucoPublisher.publish(arucoID)
-#                 self.get_logger().info('Sent marker: "%s"' % arucoID.data)
-#                 self.done = 1
-#             elif self.mode == 2 and self.done == 0:
-                
-
-
-
-#                 self.done = 1
-#             elif self.mode == 3 and self.done == 0:
-#                 self.previousID = arucoID
-
-
-    
-# def main(args=None):
-#     rclpy.init(args=args)
-#     node = BVMNode()
-#     try:
-#         rclpy.spin(node)
-#     except:
-#         KeyboardInterrupt
-# if __name__ == '__main__':
-#     main()
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool, Int8
-#drone battery 3-9
-#upper battery 1-3
-#lower battery 4-7
+import serial
+import time
+import struct
+#drone battery 4-5
+#battery columns 0-3
 #we get drone landed done from gcs node (mode 1)
 #voltage from esp and find lowest aruco with no voltage
 #find max voltage battery lowest aruco
@@ -138,9 +21,9 @@ class BVMNode(Node):
         self.mode = 0
         self.done = 0 # flag for each mode
         self.checkModeComplete = False
-        self.DroneMarkers = [7, 8]
-        self.arucoID = None
-
+        self.DroneMarkers = [4, 5]
+        self.batteryChamber = None
+        self.ser = serial.Serial('/dev/ttyUSB1', 115200, timeout=1)
 
 
         self.arucoPublisher = self.create_publisher(
@@ -150,14 +33,14 @@ class BVMNode(Node):
         self.armSubscriber = self.create_subscription(
         Bool, 'modeComplete', self.modeComplete, 10)
 
-        self.arucoIDPublisher()
+        # self.arucoIDPublisher()
 
         # Uncomment line and delete arucoID() once finished with GCS node
-        # self.bvmLogic()
+        self.run_timer = self.create_timer(0.1, self.bvmLogic)
     
     def arucoIDPublisher(self):
         msg = Int8()
-        msg.data = int(input("Input Aruco Marker: "))
+        msg.data = int(input("Input Battery Chamber: "))
         self.arucoPublisher.publish(msg)
         self.get_logger().info('Sent marker: "%s"' % msg.data)
 
@@ -170,21 +53,65 @@ class BVMNode(Node):
     
     def batteryAmount(self, msg):
         self.batteries = msg.data
-    
+
+    def structUnpack(self, type, data):
+        try:
+            return [round(v,2) for v in struct.unpack(type, data)]
+        except:
+            self.get_logger().info("Incomplete data received")
+
+    def espRead(self):
+        if self.ser.in_waiting > 0:
+            tag = self.ser.readline().decode('utf-8').strip()
+            # Examples of reading from ESP
+            if tag == "Voltage":
+                raw_data = self.ser.read(32)
+                values = self.structUnpack('8f', raw_data)
+                valid_data = sum(1 for v in values if v > 3.0)
+
+                if valid_data >= 1:
+                    self.get_logger().warn("Valid data received")
+                    time.sleep(1)
+                    self.batteryChamber = values.index(max(values))
+                    self.get_logger().warn(f"Choosing chamber {self.batteryChamber}.")
+                    self.mode = 1
+                else:
+                    self.get_logger().warn("Invalid voltage data received: Less than 7 charged batteries read.")
+
+            elif tag == "OtherInfo":
+                raw_data = self.ser.read(12)
+                values = self.structUnpack('3i', raw_data)
+            else:
+                return
+            self.get_logger().info("Tag: " + tag)
+            self.get_logger().info(f"{values}")
+
+    def espSend(self, tag, data):
+        self.ser.write((f"{tag}").encode('utf-8'))
+        if isinstance(data, list):
+            self.ser.write(bytearray(data), len(data))
+        else:
+            data = str(data)
+            self.ser.write(data.encode('utf-8'))
+
+
+        
     def bvmLogic(self):
         if len(self.DroneMarkers) > 0:
             if self.mode == 0:
-                pass
+                self.espRead()
             elif self.mode == 1 and self.done == 0:
                 
+                self.espSend("Chamber", self.batteryChamber)
+
 
 
 
                 # Get BVM aruco ID needed
-                arucoID = Int8() 
-                arucoID.data = None # Figure out how to get it from esp
-                self.arucoPublisher.publish(arucoID)
-                self.get_logger().info('Sent marker: "%s"' % arucoID.data)
+                # arucoID = Int8() 
+                # arucoID.data = None # Figure out how to get it from esp
+                # self.arucoPublisher.publish(arucoID)
+                # self.get_logger().info('Sent marker: "%s"' % arucoID.data)
                 self.done = 1
             elif self.mode == 2 and self.done == 0:
                 
@@ -192,7 +119,7 @@ class BVMNode(Node):
 
 
                 self.done = 1
-            elif self.mode == 3:
+            elif self.mode == 3 and self.done == 0:
                 self.previousID = arucoID
 
 
@@ -204,7 +131,5 @@ def main(args=None):
         rclpy.spin(node)
     except:
         KeyboardInterrupt
-    finally:
-        rclpy.shutdown()
 if __name__ == '__main__':
     main()
--- Banshee-ros/src/stepper_control/stepper_control/stepper_node.py
+++ Banshee-ros/src/stepper_control/stepper_control/stepper_node.py
@@ -1,108 +1,144 @@
 # import rclpy
 # from rclpy.node import Node
-# from std_msgs.msg import Bool, Float32, Int8
-# # import RPi.GPIO as GPIO
-# from Arduino import Arduino
+# from std_msgs.msg import Bool, Int32, Int8
+# import RPi.GPIO as GPIO
 # from time import sleep
 
 # # Pin Definitions
 # DIR = 10  # Direction Pin (Dir+)
 # STEP = 8  # Step Pin (Pul+)
 
-# CW = "HIGH"   # Clockwise Rotation
-# CCW = "LOW"  # Counter Clockwise Rotation
+# CW = 1   # Clockwise Rotation
+# CCW = 0  # Counter Clockwise Rotation
 
 # # Set Positions in BTP
-# COLUMN1 = 500
+# COLUMN1 = 463
 # COLUMN2 = 1481
 # COLUMN3 = 2492
 # COLUMN4 = 3510
 
+# DELAY = 0.003
 
 # class StepperMotorNode(Node):
 #     def __init__(self):
 #         super().__init__('stepper_motor_node')
         
-#         # GPIO.setmode(GPIO.BOARD)
-#         # GPIO.setup(DIR, GPIO.OUT)
-#         # GPIO.setup(STEP, GPIO.OUT)
-
-#         self.board = Arduino(115200, port="/dev/ttyS1")
-#         self.board.pinMode(DIR, "OUTPUT")
-#         self.board.pinMode(STEP, "OUTPUT")
+#         # GPIO Setup
+#         GPIO.setmode(GPIO.BOARD)
+#         GPIO.setup(DIR, GPIO.OUT)
+#         GPIO.setup(STEP, GPIO.OUT)
         
-#         self.stepCoefficient = 500/159.5
+#         # Attributes
+#         self.arucoID = None
+#         self.distance = None
+#         self.stepCoefficient = 1000 / 296  # Conversion factor for distance to steps
 #         self.position = 0
         
 #         # ROS2 Publisher and Subscribers
 #         self.initialSubscription = self.create_subscription(
-#         Int8, 'arucoID', self.initialSubscriber, 10)
+#             Int8, 'arucoID', self.initialSubscriber, 10)
 
 #         self.distanceSubscription = self.create_subscription(
-#         Float32, 'DestinationFalse', self.distanceSubscriber, 10)
+#             Int32, 'DestinationFalse', self.distanceSubscriber, 10)
 
 #         self.done_publisher = self.create_publisher(Bool, 'stepperDone', 10)
 
 #         self.get_logger().info('Stepper Motor Node has been started and is ready for commands.')
 
 #     def initialSubscriber(self, msg):
-#         arucoID = msg.data
-#         self.get_logger().info(f"Received Aruco ID: {arucoID}")
-
-#         aruco_to_column = {
-#                 0: COLUMN1, 4: COLUMN1,
-#                 1: COLUMN2, 5: COLUMN2,
-#                 2: COLUMN3, 6: COLUMN3,
-#                 3: COLUMN4, 7: COLUMN4
-#             }
-#         self.run_motor_cycle(aruco_to_column.get(arucoID, None))
+#         """Callback for receiving Aruco IDs."""
+#         self.arucoID = msg.data
+#         self.get_logger().info(f"Received Aruco ID: {self.arucoID}")
+#         self.initial_movement()
 
 #     def distanceSubscriber(self, msg):
-#         distance = msg.data
-#         self.get_logger().info(f"Received distance: {distance}")
-#         addedSteps = distance * self.stepCoefficient
-#         self.run_motor_cycle(self.position + addedSteps)
+#         """Callback for receiving distance to move the motor."""
+#         self.distance = msg.data
+#         self.get_logger().info(f"Received distance: {self.distance}")
+#         self.run_motor_cycle()
 
-#     def run_motor_cycle(self, newPosition):
+#     def initial_movement(self):
+#         """Move the stepper motor to the column associated with the received Aruco ID."""
 #         try:
-#             if newPosition is not None:
-#                 self.get_logger().info(str(newPosition) + " is new position")
+#             if self.arucoID is None:
+#                 self.get_logger().warn("Aruco ID not set. Skipping initial movement.")
+#                 return
 
-#                 steps = newPosition - self.position
+#             # Mapping Aruco IDs to columns
+#             aruco_to_column = {
+#                 0: COLUMN4, 1: COLUMN4,
+#                 2: COLUMN3, 3: COLUMN3,
+#                 4: COLUMN2, 5: COLUMN2,
+#                 6: COLUMN1, 7: COLUMN1
+#             }
 
-#                 # Max steps in CW 4050
-#                 if self.position + steps > 4050 or self.position + steps < 0:
+#             # Determine the target column based on the received Aruco ID
+#             target_steps = aruco_to_column.get(self.arucoID, None)
+
+#             if target_steps is None:
+#                 self.get_logger().warn(f"Invalid Aruco ID: {self.arucoID}. Skipping movement.")
+#                 return
+
+#             self.get_logger().info(f"Moving to column associated with Aruco ID {self.arucoID}.")
+
+#             # Move to the target column
+#             GPIO.output(DIR, CW)
+#             for _ in range(target_steps):
+#                 GPIO.output(STEP, GPIO.HIGH)
+#                 sleep(DELAY)
+#                 GPIO.output(STEP, GPIO.LOW)
+#                 sleep(DELAY)
+
+#             # Return to origin
+#             GPIO.output(DIR, CCW)
+#             for _ in range(target_steps):
+#                 GPIO.output(STEP, GPIO.HIGH)
+#                 sleep(DELAY)
+#                 GPIO.output(STEP, GPIO.LOW)
+#                 sleep(DELAY)
+
+#             sleep(1)
+#             self.get_logger().info('Cycle complete, publishing signal to camera.')
+#             cycle_complete_msg = Bool()
+#             cycle_complete_msg.data = True
+#             self.done_publisher.publish(cycle_complete_msg)
+
+#         except KeyboardInterrupt:
+#             self.cleanup()
+
+#     def run_motor_cycle(self):
+#         """Move the motor based on the received distance."""
+#         try:
+#             if self.distance is not None:
+#                 if self.distance > 4050 or self.distance < -4050:
 #                     self.get_logger().warn("Distance out of range. Movement skipped.")
 #                     return
 
-#                 if steps > 0:
-#                     self.board.digitalWrite(DIR, CW)
-#                     # GPIO.output(DIR, CW)
-#                 else:
-#                     self.board.digitalWrite(DIR, CCW)
-#                     # GPIO.output(DIR, CCW)
-#                 for _ in range(abs(round(steps))):   
-#                     self.board.digitalWrite(STEP, "HIGH")                
-#                     # GPIO.output(STEP, GPIO.HIGH)
-#                     sleep(0.002) 
-#                     self.board.digitalWrite(STEP, "LOW")
-#                     # GPIO.output(STEP, GPIO.LOW)
-#                     sleep(0.002)
-                
-#                 self.position = self.position + steps
+#                 GPIO.output(DIR, CW if self.distance > 0 else CCW)
+
+#                 steps = round(abs(self.stepCoefficient * self.distance))
+#                 self.get_logger().info(f"Running motor cycle for {steps} steps.")
+
+#                 for _ in range(steps):
+#                     GPIO.output(STEP, GPIO.HIGH)
+#                     sleep(DELAY)
+#                     GPIO.output(STEP, GPIO.LOW)
+#                     sleep(DELAY)
 
 #                 sleep(1)
 #                 # Publish cycle complete signal
-#                 self.get_logger().info('Cycle complete, publishing signal to camera')
+#                 self.get_logger().info('Cycle complete, publishing signal to camera.')
 #                 cycle_complete_msg = Bool()
 #                 cycle_complete_msg.data = True
 #                 self.done_publisher.publish(cycle_complete_msg)
+#                 self.distance = None
 #         except KeyboardInterrupt:
 #             self.cleanup()
 
 #     def cleanup(self):
-#         self.get_logger().info('Cleaning up...')
-#         # GPIO.cleanup()
+#         """Cleanup GPIO on shutdown."""
+#         self.get_logger().info('Cleaning up GPIO...')
+#         GPIO.cleanup()
 
 
 # def main(args=None):
@@ -121,23 +157,23 @@
 
 # if __name__ == '__main__':
 #     main()
+    
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool, Float32, Int8
 # import RPi.GPIO as GPIO
-from time import sleep 
-from pinpong.board import Board, Pin
-Board("leonardo", "/dev/ttyACM0").begin()  # Initialization with specified port on Linux
-# Pin Definitions
-DIR = Pin.D10  # Direction Pin (Dir+)
-STEP = Pin.D8  # Step Pin (Pul+)
+from Arduino import Arduino
+from time import sleep
+import math
 
-CW = 1   # Clockwise Rotation
-CCW = 0  # Counter Clockwise Rotation
+# Pin Definitions
+DIR = 10  # Direction Pin (Dir+)
+STEP = 8  # Step Pin (Pul+)
+
+CW = "HIGH"   # Clockwise Rotation
+CCW = "LOW"  # Counter Clockwise Rotation
 
 # Set Positions in BTP
-DRONE = 1481
-RESET = 100
 COLUMN1 = 500
 COLUMN2 = 1481
 COLUMN3 = 2492
@@ -147,9 +183,15 @@ COLUMN4 = 3510
 class StepperMotorNode(Node):
     def __init__(self):
         super().__init__('stepper_motor_node')
-        self.rotation = Pin(DIR, Pin.OUT)  # Initialize the pin for digital output
-        self.movement = Pin(STEP, Pin.OUT)
+        
+        # GPIO.setmode(GPIO.BOARD)
+        # GPIO.setup(DIR, GPIO.OUT)
+        # GPIO.setup(STEP, GPIO.OUT)
 
+        self.board = Arduino(115200, port="/dev/ttyS1")
+        self.board.pinMode(DIR, "OUTPUT")
+        self.board.pinMode(STEP, "OUTPUT")
+        
         self.stepCoefficient = 500/159.5
         self.position = 0
         
@@ -172,8 +214,7 @@ class StepperMotorNode(Node):
                 0: COLUMN1, 4: COLUMN1,
                 1: COLUMN2, 5: COLUMN2,
                 2: COLUMN3, 6: COLUMN3,
-                3: COLUMN4, 7: COLUMN4,
-                8: DRONE
+                3: COLUMN4, 7: COLUMN4
             }
         self.run_motor_cycle(aruco_to_column.get(arucoID, None))
 
@@ -183,12 +224,18 @@ class StepperMotorNode(Node):
         addedSteps = distance * self.stepCoefficient
         self.run_motor_cycle(self.position + addedSteps)
 
+    def speedCoefficient(self, steps, i):
+        if steps < 300:
+            return -0.5 * pow(math.e, -pow((5(i / steps) - 2), 2)) + 1
+
+
     def run_motor_cycle(self, newPosition):
         try:
             if newPosition is not None:
                 self.get_logger().info(str(newPosition) + " is new position")
 
                 steps = newPosition - self.position
+                abs_rounded_steps = abs(round(steps))
 
                 # Max steps in CW 4050
                 if self.position + steps > 4050 or self.position + steps < 0:
@@ -196,18 +243,21 @@ class StepperMotorNode(Node):
                     return
 
                 if steps > 0:
-                    self.rotation.write_digital(CW)
+                    self.board.digitalWrite(DIR, CW)
                     # GPIO.output(DIR, CW)
                 else:
-                    self.rotation.write_digital(CCW)
+                    self.board.digitalWrite(DIR, CCW)
                     # GPIO.output(DIR, CCW)
-                for _ in range(abs(round(steps))):   
-                    self.movement.write_digital(1)         
+                for i in range(abs_rounded_steps):   
+                    speed = self.speedCoefficient(abs_rounded_steps, i)
+
+
+                    self.board.digitalWrite(STEP, "HIGH")
                     # GPIO.output(STEP, GPIO.HIGH)
-                    sleep(0.002) 
-                    self.movement.write_digital(0)
+                    sleep(0.002 * speed) 
+                    self.board.digitalWrite(STEP, "LOW")
                     # GPIO.output(STEP, GPIO.LOW)
-                    sleep(0.002)
+                    sleep(0.002 * speed)
                 
                 self.position = self.position + steps
 
