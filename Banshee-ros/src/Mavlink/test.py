c7c58f0bd1b270d9a86e193887be069c6d3d191a
Cjoshua28 <tinyjoshua28@gmail.com>
2025-02-26 17:21:22 -0800

    test

--- Banshee-ros/src/Mavlink/test.py
+++ Banshee-ros/src/Mavlink/test.py
@@ -2,7 +2,7 @@ import time
 import sys
 from pymavlink import mavutil
 
-master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+master = mavutil.mavlink_connection("/dev/ttyAMA0", baud=115200)
 master.wait_heartbeat()
 print("HEARTBEAT_DONE")
  
