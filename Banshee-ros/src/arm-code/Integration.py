2e212a74e9d74d206121b784e1ab5777cb97836d
galgim <claeekim@gmail.com>
2025-02-27 13:15:31 -0800

    clay

--- Banshee-ros/src/arm-code/Integration.py
+++ Banshee-ros/src/arm-code/Integration.py
@@ -52,7 +52,7 @@ def Pull_low():
     print("Pull out low sequence")
     motor.dxlSetVelo([30, 35, 25, 35], [1, 2, 3, 4])            # set initial velocity
     print("remove chamber")
-    motor.simMotorRun([80, 248, 95],[2, 3, 4])                  # move arm to chamber position
+    motor.simMotorRun([80, 247, 95],[2, 3, 4])                  # move arm to chamber position
     Close()                                                     # grab battery
     motor.dxlSetVelo([17, 37, 20], [2, 3, 4])                   # set pull out velocity
     motor.simMotorRun([132, 343, 40],[2, 3, 4])                 # pull battery out
