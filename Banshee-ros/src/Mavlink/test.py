854f1d0a31175da0c61f2bc7b024f9c445884382
galgim <claeekim@gmail.com>
2025-03-03 13:33:07 -0800

    mavlink test

--- Banshee-ros/src/Mavlink/test.py
+++ Banshee-ros/src/Mavlink/test.py
@@ -1,15 +1,24 @@
 import time
-import sys
 from pymavlink import mavutil
 
-master = mavutil.mavlink_connection("/dev/ttyAMA0", baud=115200)
-master.wait_heartbeat()
-print("HEARTBEAT_DONE")
- 
-master.param_fetch_all()
- 
-while True:
-            m = master.recv_match(type = "PARAM_VALUES", blocking = True, timeout = 1)
-            print(m)
-            if m is None:
-                        break
+master = mavutil.mavlink_connection('/dev/serial/by-id/usb-Holybro_Pixhawk6C_1E0030001151333036383238-if00', baud=115200)
+
+master.arducopter_arm()
+
+print('arm')
+
+master.mav.command_long_send(
+    1, 0,
+    mavutil.mavlink.MAV_CMD_DO_MOTOR_TEST, 0,
+    1, 0, 20, 2, 0, 0, 0
+)
+
+
+
+time.sleep(5)
+
+master.arducopter_disarm()
+
+print('done')
+
+master.close()
