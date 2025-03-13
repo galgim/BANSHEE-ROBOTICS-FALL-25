29d98503e72ed19c743c1aec07e06f274a08b933
MaherSalem7 <maher.salem5@gmail.com>
2025-03-13 15:24:50 -0700

    test

--- Banshee-ros/src/integration/integration/integration_node.py
+++ Banshee-ros/src/integration/integration/integration_node.py
@@ -156,7 +156,7 @@ def Drone_pull():
     motor.simMotorRun([95, 243, 112],[2, 3, 4])                 # move to battery position
     Close()  
     motor.simMotorRun([154, 334, 90],[2, 3, 4])                 # get to middle position
-    motor.dxlSetVelo([25, 15, 25], [2, 3, 4])                    # set grab speed                      
+    motor.dxlSetVelo([30, 25, 15, 25], [1, 2, 3, 4])                    # set grab speed                      
     motor.simMotorRun([222, 334, 139],[2, 3, 4])                # pull battery out
     motor.simMotorRun([225], [1])                   
 
