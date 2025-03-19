c15836b92668ef7098b37fdc8690b19b409d1007
galgim <claeekim@gmail.com>
2025-03-18 19:41:55 -0700

    Test

---
+++ Banshee-ros/src/bvm_node/bvm_node/spi_raspberry.py
@@ -0,0 +1,17 @@
+import spidev
+import time
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 500000
+spi.mode = 0
+
+
+try:
+    while True:
+        send_byte = 0x26
+        received_byte = spi.xfer2([send_byte])[0]
+        print(f"Sent: 0x{send_byte:X}, Received: 0x{received_byte:X}")
+        time.sleep(1)
+except KeyboardInterrupt:
+    spi.close()
