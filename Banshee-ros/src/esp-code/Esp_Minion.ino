756cb3115cefdf93cbf44be35251e063f66d80e2
YeachanOkh <yeachanokh@gmail.com>
2025-03-26 13:16:20 -0700

    test

--- Banshee-ros/src/esp-code/Esp_Minion.ino
+++ Banshee-ros/src/esp-code/Esp_Minion.ino
@@ -1,3 +1,104 @@
+VoidMango
+voidmango
+Online
+
+VoidMango — 10:17 AM
+what the heck
+Gimgal — 10:17 AM
+that was so random
+Gimgal — 11:09 AM
+Image
+Image
+Image
+VoidMango — 11:11 AM
+i dont think that cube has any extra ports
+for electronics
+hmmm
+Gimgal — 11:12 AM
+honestly
+battery swapping looks impossible
+without outside mounting
+VoidMango — 11:12 AM
+it is impossible
+i told you the bs needs to go
+lol
+Gimgal — 11:12 AM
+LOL
+i see these guys really want this to work
+VoidMango — 11:12 AM
+no theyre scope too big rn we havent even worked on the sinch drone yet
+so i would really emphasize on the sinch drone getting done first before moving on
+Gimgal — 11:14 AM
+ill try to be clear with that
+VoidMango — 11:14 AM
+dang taking initiative already
+lol
+Gimgal — 11:14 AM
+i gots to
+i met the new aero lead
+VoidMango — 11:15 AM
+ill get you guys cuaght up on ee stuff coming next months
+who
+Gimgal — 11:15 AM
+nathan
+VoidMango — 11:15 AM
+oh i see
+what you think of him
+Gimgal — 11:15 AM
+hes gonna be aero next year
+VoidMango — 11:15 AM
+?
+Gimgal — 11:15 AM
+i thjnk
+he just like me LOL
+VoidMango — 11:15 AM
+lol
+Gimgal — 11:15 AM
+we both kida clueless
+VoidMango — 11:15 AM
+wdym by that
+ahahaha
+thats usually how it goes
+Gimgal — 11:15 AM
+but he seems chill
+have to see how he works
+also is christian a new lead too?
+VoidMango — 11:16 AM
+most of the time you wont be working with aero but we shall see
+yeah so you should get to know him more
+Gimgal — 11:16 AM
+will do
+he ee lead?
+VoidMango — 11:16 AM
+no hes comms lead
+Gimgal — 11:16 AM
+o rught
+VoidMango — 11:17 AM
+ee lead is some aero guy thats also clue less
+Gimgal — 11:17 AM
+LOL
+at least i have josh
+VoidMango — 11:19 AM
+you have josh so thats good
+hes smarter than me so you should be fine 
+just make sure he doenst do everything by himself 
+gotta split work up
+Gimgal — 11:20 AM
+got it
+VoidMango — 1:15 PM
+can you send your minion over
+code
+Gimgal — 1:15 PM
+#include <WiFi.h>
+#include <esp_now.h>
+#include <Arduino.h> 
+#include "esp_adc_cal.h"
+
+// Define the GPIO pin to control the solenoid
+Expand
+message.txt
+5 KB
+﻿
 #include <WiFi.h>
 #include <esp_now.h>
 #include <Arduino.h> 
