f9dd7a3cd0cd9fdf686bfc52ad775796002e96e6
galgim <claeekim@gmail.com>
2025-04-17 15:07:15 -0700

    test

--- Banshee-ros/src/integration/integration/Integration.py
+++ Banshee-ros/src/integration/integration/Integration.py
@@ -66,7 +66,7 @@ def Push_high():
     print("place chamber")
     motor.simMotorRun([130, 310, 85],[2, 3, 4])                   # move arm to first position
     time.sleep(0.5)
-    motor.dxlSetVelo([20, 40, 15], [2, 3, 4])                    # set final velocity (new)
+    motor.dxlSetVelo([20, 40, 10], [2, 3, 4])                    # set final velocity (new)
     motor.simMotorRun([83, 218, 123],[2, 3, 4])                   # move arm to chamber (new)
     Open()                                                        # let go
 
