ffbeaf43c363c262d0ee88f5e018fd4fe28e624f
galgim <claeekim@gmail.com>
2025-04-17 14:46:55 -0700

    test

--- Banshee-ros/src/integration/integration/Integration.py
+++ Banshee-ros/src/integration/integration/Integration.py
@@ -66,7 +66,7 @@ def Push_high():
     print("place chamber")
     motor.simMotorRun([130, 310, 90],[2, 3, 4])                   # move arm to first position
     time.sleep(0.5)
-    motor.dxlSetVelo([23, 72, 13], [2, 3, 4])                    # set final velocity (new)
+    motor.dxlSetVelo([20, 40, 10], [2, 3, 4])                    # set final velocity (new)
     motor.simMotorRun([83, 218, 123],[2, 3, 4])                   # move arm to chamber (new)
     Open()                                                        # let go
 
@@ -78,7 +78,7 @@ def Pull_high():
     motor.simMotorRun([83, 218, 123],[2, 3, 4])                   # move arm to chamber (new)
     Close()                                                       # grab battery
     print("remove chamber")
-    motor.dxlSetVelo([23, 82, 33], [2, 3, 4])                    # set pull out velocity (new)
+    motor.dxlSetVelo([20, 60, 30], [2, 3, 4])                    # set pull out velocity (new)
     motor.simMotorRun([130, 320, 70],[2, 3, 4])                   # middle position
     startsetup()
     #Droneside()
