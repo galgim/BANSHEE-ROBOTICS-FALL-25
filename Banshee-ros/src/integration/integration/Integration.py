2ab78a604e8e9e0968e717c6d61523e989d09f57
galgim <claeekim@gmail.com>
2025-04-17 15:49:33 -0700

    test

--- Banshee-ros/src/integration/integration/Integration.py
+++ Banshee-ros/src/integration/integration/Integration.py
@@ -41,7 +41,7 @@ def Push_low():
     motor.simMotorRun([105, 330, 45],[2, 3, 4])                   # move arm to first position (new)
     time.sleep(.5)
     motor.dxlSetVelo([20, 20, 15], [2, 3, 4])                     # set final velocity
-    motor.simMotorRun([90, 260, 93],[2, 3, 4])                    # move arm to chamber position (new)
+    motor.simMotorRun([88, 258, 92],[2, 3, 4])                    # move arm to chamber position (new)
     Open()                                                        # let go of battery
 
 # Pull Battery into low BVM
